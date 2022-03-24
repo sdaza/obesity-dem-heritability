707771eb06b33cc689865b2b0a34d9bcd40b7af6
Sebastian Daza <sebastian.daza@gmail.com>
2022-03-24 16:36:11 +0100

    update

--- src/testing/testing_sensitivity.R
+++ src/testing/testing_sensitivity.R
@@ -8,3 +8,19 @@ order <- "second"
 R <- 10 ^ 3
 type <- "percent"
 conf <- 0.95
+
+
+# read heritability
+dat = fread("models/BMI-fertility/data/heritability-ff.csv")
+
+dat[, father := father - 1]
+dat[, mother := mother - 1]
+dat[, kid := kid - 1]
+fwrite(dat, "models/BMI-fertility/data/heritability-ff.csv", row.names = FALSE)
+
+dat = fread("models/BMI-fertility/data/heritability-ifls.csv")
+
+dat[, father := father - 1]
+dat[, mother := mother - 1]
+dat[, kid := kid - 1]
+fwrite(dat, "models/BMI-fertility/data/heritability-ifls.csv", row.names = FALSE)
